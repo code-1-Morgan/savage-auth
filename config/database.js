7992fe2a6846ab25e98b4f3f5d4aea17e4b3caf5
mdowning96 <morganodowning@gmail.com>
2022-11-06 19:24:41 -0500

    updated code

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    // 'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    // 'dbName': 'demo'
+
+    'url' : 'mongodb+srv://mdoBackend421:demodemo@cluster0.dcmy4kq.mongodb.net/savauth  ?retryWrites=true&w=majority',
+    'dbName' : 'savauth'
 };
